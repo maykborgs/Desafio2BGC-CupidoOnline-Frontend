154d30dfdcfcfa2d63e5a91b40878d473fb8c948
maykborgs <maaykon51@gmail.com>
2022-03-08 02:25:16 -0300

    Style: changes some styles in the home page

--- cupidoonlineappfront/src/views/Home.js
+++ cupidoonlineappfront/src/views/Home.js
@@ -6,15 +6,19 @@ function Home() {
   return(
   <div>
     <header className="header">
+      <div>
       <h1 id="firstitemheader">Cupido Online</h1>
-      <h1>Mensagens</h1>
-      <h1>Login</h1>
+      <img alt="hearticon" src={require("../static/hearticon.png")}/>
+      </div>
+      <div>
+      <h1>Cadastre-se</h1>
+      <h1>Login</h1>  
+      </div>
     </header>
     <main>
       <form>
-      <p>Aqui você pode enviar uma mensagem anônima para aquele crush que você está de   
-      <img alt="eyesemoji"src="http://cdn.shopify.com/s/files/1/1061/1924/products/Eyes_Emoji_grande.png?v=1571606063"></img>
-      </p>
+      <p>Aqui você pode enviar uma mensagem anônima para aquele crush que você está de 👀</p>
+      {/* <img alt="eyesemoji"src="http://cdn.shopify.com/s/files/1/1061/1924/products/Eyes_Emoji_grande.png?v=1571606063"></img> */}
       <label>Nome: </label>  
       <input placeholder="Digite seu nome"></input>
       <label type="email">E-mail: </label>  
